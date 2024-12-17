bee1b6fb96324553505d005411c11b1f4f966403
edsonnetoo <edson.creeper@gmail.com>
2024-12-17 12:41:50 -0300

    Adcionando verficação de arquivo

--- scripts.js
+++ scripts.js
@@ -4,3 +4,23 @@ const inputUpload = document.getElementById("imagem-upload");
 uploadBtn.addEventListener("click", () => {
     inputUpload.click();
 })
+
+//verficação de tamanho e tipo
+inputUpload.addEventListener('change', function(event) {
+    var arquivo = event.target.files[0];
+
+    if (!arquivo.type.match("image/")) {
+        alert("Por favor, inserir uma Imagem!");
+        inputUpload.value = "";
+        return;
+    }
+
+    if (arquivo.size > 2 * 1024 * 1024) {
+        alert("A Imagem deve ter no máximo 2MB");
+        inputUpload.value = "";
+        return;
+    }
+
+    console.log("Olha a pedra", arquivo);
+})
+
